d20d83528f36d7f34c2c69adc819b2f1943d4125
Keitel Jovin <keiteldog@gmail.com>
2019-09-11 21:24:39 -0700

    Implement GET groduct reviews API

--- controllers/review.js
+++ controllers/review.js
@@ -1,6 +1,45 @@
 require('../db/index.js');
 const Reviews = require('../models/Reviews');
 
+const sumRatings = (reviews) => reviews.reduce((acc, review) => {
+  const keys = Object.keys(review.ratings);
+  keys.forEach((key) => {
+    if (!acc[key]) {
+      acc[key] = review.ratings[key];
+    } else {
+      acc[key] += review.ratings[key];
+    }
+  });
+  return acc;
+}, {});
+
+const getSummary = (reviews) => {
+  const totalRecommends = reviews.filter((review) => review.recommend === true);
+  const totals = sumRatings(reviews);
+  const keys = Object.keys(totals);
+  const summary = keys.reduce((acc, key) => {
+    const temp = totals[key] / reviews.length;
+    acc[key] = Number.parseFloat(temp).toFixed(1);
+    return acc;
+  }, {});
+  delete summary.$init;
+  Object.assign(summary, { recommends: totalRecommends.length, reviews: reviews.length });
+  return summary;
+};
+
+const getReviews = (req, res) => {
+  const productId = getProductId(req);
+  console.log('productid', productId);
+  Reviews.find({ productId })
+    .then((reviews) => {
+      const results = {};
+      results.summary = getSummary(reviews);
+      results.reviews = reviews;
+      res.status(200).json(results);
+    })
+    .catch((err) => res.status(404).send(err.message));
+};
+
 
 const create = (req, res, next) => {
   var reviewData = req.body;
@@ -8,16 +47,29 @@ const create = (req, res, next) => {
   Reviews.create(reviewData, (err, data) => {
     if (err) {
       console.log('create error', err);
+      res.status(400).json({ success: false, message: 'Could not save review to Database' });
       return;
+    } else {
+      res.status(201).json({success: true});
     }
-
-    res.status(201).send({success: true});
   });
 
 };
 
 const findFromProduct = (req, res, next) => {
+  var pId = req.params.productId;
 
+  Reviews.find({ productId: pId }, (err, reviews) => {
+    if (err) {
+      console.log('error fetching product reviews', err);
+      res.status(400).json({ success: false, message: 'Could not fetch product reviews from our Database' });
+    } else {
+      const results = {};
+      results.summary = getSummary(reviews);
+      results.reviews = reviews;
+      res.status(200).json(results);
+    }
+  });
 };
 
 const findOne = (req, res, next) => {
